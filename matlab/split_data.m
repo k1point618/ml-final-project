2b8d9733ef9e44e171abef9c1e4220f45ee369b3
Keren Gu <keren.gu.10@gmail.com>
2013-11-29 04:44:11 -0500

    add shuffled data and be able to split data into training and test data set after shuffling with a given percentage of the training set.

---
+++ matlab/split_data.m
@@ -0,0 +1,30 @@
+function [ trainX, trainY, testX, testY ] = split_data(percent_training, X, Y)
+ % Takes in X that is d x M; Y that is M x 1. 
+ % X's are features of dimention d;
+ % Y's are the M labels.
+ % Returns:
+ % trainX is the first N columns of the permutated feature vectors.
+ %   d x N
+ % trainY is the corresponding label of the training data, trainX. 
+ %   N x 1
+ % testX is the last M-N columns of the permutated feature vectors.
+ %   d x (M-N)
+ % testY is the corresponding label of the test data, testX. 
+ %   (M-N) x 1
+ 
+M = size(Y, 1);
+
+perm = randperm(M);
+
+shuffX = X(:, perm);
+shuffY = Y(perm, :);
+
+N = floor(percent_training * M);
+
+trainX = shuffX(:,1:N);
+trainY = shuffY(1:N,:);
+testX = shuffX(:,N:end);
+testY = shuffY(N:end,:);
+
+end
+
