8bdce0a3e3756a8f229714922f1a17ebdac742c8
Ernest Zeidman <ezeidman@gmail.com>
2013-11-30 16:14:49 -0500

    refactoring test.m

--- matlab/test.m
+++ matlab/test.m
@@ -1,10 +1,11 @@
 function [ test_error ] = test(trainX,trainY,model,testX,testY)
-    errors = arrayfun( @(i) evaluate(trainX, trainY, model, testX(i)) == testY(i),...
+    agreement = arrayfun( @(i) evaluate(trainX, trainY, model, testX(i)) * testY(i),...
                         1: length(testY));
+    minimum_agreement = min(agreement)
+    errors = arrayfun(@(a) a <= 0, agreement);
     test_error = sum(errors)/length(errors);                
 end
 
 function [ y ] = evaluate(trainX, trainY, model, x)
-    f = sum(arrayfun( @(i) trainY(i)*model.alpha(i)*model.kernel(trainX(i),x), 1:length(trainY)));
-    y = sign(f);
+    y = sum(arrayfun( @(i) trainY(i)*model.alpha(i)*model.kernel(trainX(i),x), 1:length(trainY)));
 end
