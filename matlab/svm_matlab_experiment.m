2a2000112ee055dcc516bec47ed51541566fd59d
Keren Gu <keren.gu.10@gmail.com>
2013-12-01 23:33:33 -0500

    add svm matlab experiement to use LOOCV

--- matlab/svm_matlab_experiment.m
+++ matlab/svm_matlab_experiment.m
@@ -2,45 +2,29 @@ load('data.mat');
 Y = after_day_labels;
 X = news;
 
-NUM_ITERATION = 5;
-betas = [0.015:0.005:0.05 0.6:0.1:1];
-C = 1;
+betas = [0.001:0.001:0.1];
+%C = [0.01:0.05:1];
 
-avg_test_errors = zeros(2, length(betas));
-avg_train_errors = zeros(2, length(betas));
+Variable = betas;
 
-for j=1:length(betas)
+avg_test_errors = zeros(1, length(Variable));
+avg_train_errors = zeros(1, length(Variable));
 
+for j=1:length(Variable)
     
+    betas(j)
     test_errors = zeros(1, NUM_ITERATION);
     train_errors = zeros(1, NUM_ITERATION);
 
-    for i=1:NUM_ITERATION
-        [ trainX, trainY, testX, testY ] = split_data(X, sign(Y+0.00001), .85);
-
-        SVMstruct = svmtrain(trainX,trainY,'Kernel_Function','rbf', ...
-            'rbf_sigma', betas(j), 'boxconstraint', C);
-        
-        train_errors(i) = sum((svmclassify(SVMstruct,trainX).*trainY)<0)/length(trainY);
-        test_errors(i) = sum((svmclassify(SVMstruct,testX).*testY)<0)/length(testY);
+    [avg_test_errors(j), avg_train_errors(j)] = LOOCV(X, sign(Y+0.00001), 1, betas(j));
     
-%         model = svm_build(trainX, trainY, C, betas(j));
-%         
-%         train_errors(2, i) = test2(trainX, trainY, model, trainX, trainY);
-%         test_errors(2, i) = test2(trainX, trainY, model, testX, testY);
-
-    end
-
-    avg_test_errors(j) = sum(test_errors)/length(test_errors);
-    avg_train_errors(j) = sum(train_errors)/length(train_errors);
-
 end
     
 avg_test_errors
 avg_train_errors
 
-plot(betas, avg_test_errors);
+plot(Variable, avg_test_errors);
 hold on;
-plot(betas, avg_train_errors);
+plot(Variable, avg_train_errors);
 hold off;
 
