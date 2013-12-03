2418abf21db5d3834eabaff137875685a04f1d7f
Ernest Zeidman <ezeidman@gmail.com>
2013-12-03 18:18:29 -0500

    Making sort by time more generic

--- matlab/sort_by_time.m
+++ matlab/sort_by_time.m
@@ -1,10 +1,12 @@
 function [ sorted_news, sorted_article_labels ] = sort_by_time( news, article_labels )
 % Sorts by time
 
+m = size(article_labels,2);
+
 temp = sortrows([article_labels [1:size(article_labels, 1)]'], 4);
 
-sorted_article_labels = temp(:, 1:4);
-sorted_news = news(temp(:, 5), :);
+sorted_article_labels = temp(:, 1:m);
+sorted_news = news(temp(:, m+1), :);
 
 end
 
