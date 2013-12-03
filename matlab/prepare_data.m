2a0c0a19a94dbe35573da2367b376addfa75f61c
Ernest Zeidman <ezeidman@gmail.com>
2013-12-03 18:59:55 -0500

    Making prepare data use historical prices; updating article labels

--- matlab/prepare_data.m
+++ matlab/prepare_data.m
@@ -4,7 +4,12 @@ function [ output_args ] = prepare_data( dir )
 path1 = strcat(dir, '/article_labels.mat')
 article_labels = csvread(path1);
 
+m = size(article_labels,2);
+% article_labels(:,5:m) now gives some historical prices, which we'll
+% append to the end of the feature matrix
+
 news = csvread(strcat(dir, '/news.mat'));
+news = [news article_labels(:,5:m)];
 
 [ news, article_labels ] = sort_by_time( news, article_labels );
 
