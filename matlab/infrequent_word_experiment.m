5c13a0ee3ed48b93172fc7de4c7aefcb5a46e96a
Ernest Zeidman <ezeidman@gmail.com>
2013-12-02 20:55:17 -0500

    refactoring frequency experiments

--- matlab/infrequent_word_experiment.m
+++ matlab/infrequent_word_experiment.m
@@ -3,48 +3,31 @@
 'Infrequent Word Experiment'
 
 load('data.mat');
-X = (news > 0); % rounding for nbc 
+X = news;
 y = sign(after_day_labels + .00001);
-[ trainX2, trainY, testX, testY ] = split_data(X, y, .8);
 
 
 %  number of words
 m = size(X, 2)
+C = 1;
+beta = 3;
 
 word_sums = sum(X,1);
 sorted_words = sort(word_sums);
-
 threshold = sorted_words(floor(m*.1));
+
 
 frequent = find(word_sums > threshold);
 infrequent = find(word_sums <= threshold);
-
 length(frequent)
 length(infrequent)
 
-trials = 25
 
-test_correctness = zeros(trials,1);
-test_correctness2 = zeros(trials,1);
+X2 = X(:,frequent);
 
-for trial = 1:trials
-    
-    
-    
-    [ trainX, trainY, testX, testY ] = split_data(X, y, .8);
-    
-    trainX2 = trainX(:,frequent);
-    testX2 = testX(:,frequent);
-    
-    model1 = nbc_build(trainX, trainY);
-    model2 = nbc_build(trainX2, trainY);
-    
-    out1 = testX * model1.b' + model1.a;
-    test_correctness(trial) = sum(out1 .* testY > 0 )/length(testY);
+result1 = LOOCV( X, y, C, beta);
+result2 = LOOCV( X2, y, C, beta);
 
-    out2 = testX2 * model2.b' + model2.a;
-    test_correctness2(trial) = sum(out2 .* testY > 0 )/length(testY);
-end
-
-mean(test_correctness)
-mean(test_correctness2)
+test_error1 = result1(1)
+test_error2 = result2(1)
+    
