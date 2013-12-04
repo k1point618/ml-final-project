c80426ebca9d8031175b2d3c5740d51f0148b8bf
Ernest Zeidman <ezeidman@gmail.com>
2013-12-03 21:24:49 -0500

    Overhaul of prepare_data

--- matlab/balance_classes.m
+++ matlab/balance_classes.m
@@ -1,7 +1,7 @@
 function [ X, y ] = balance_classes( X, y )
 
-num_pos = sum(y == 1);
-num_neg = sum(y == -1);
+num_pos = sum(y > 0);
+num_neg = sum(y < 0);
 
 assert (num_pos + num_neg == length(y));
 
@@ -9,8 +9,8 @@ smaller = min(num_pos, num_neg);
 p1 = randperm(num_pos);
 p2 = randperm(num_neg);
 
-pos = find(y ==1);
-neg = find(y ==-1);
+pos = find(y > 0);
+neg = find(y < 0);
 pos = pos(p1);
 neg = neg(p2);
 
